3e749e174546dd59d0b038b268561b18ae1d670a
jaykumar1986 <jaysony143@gmail.com>
2016-03-12 00:32:38 +0400

    FbLogIn Method
    Adding login with FB methods and frameworks

---
+++ MyPID/Bridging-Header.h
@@ -0,0 +1,14 @@
+//
+//  Bridging-Header.h
+//  thepid
+//
+//  Created by Jayprakash Kumar on 3/11/16.
+//  Copyright © 2016 Jayprakash Kumar. All rights reserved.
+//
+
+#ifndef Bridging_Header_h
+#define Bridging_Header_h
+
+#import <FBSDKCoreKit/FBSDKCoreKit.h>
+#import <FBSDKLoginKit/FBSDKLoginKit.h>
+#endif /* Bridging_Header_h */
--- MyPID/ViewController.swift
+++ MyPID/ViewController.swift
@@ -8,7 +8,8 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+
+class ViewController: UIViewController , FBSDKLoginButtonDelegate {
 
     @IBAction func clickToLoginWithFaceBook(sender: AnyObject) {
         
@@ -16,6 +17,15 @@ class ViewController: UIViewController {
     }
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        if (FBSDKAccessToken.currentAccessToken() == nil)
+        {
+            let loginView : FBSDKLoginButton = FBSDKLoginButton()
+            self.view.addSubview(loginView)
+            loginView.center = self.view.center
+            loginView.readPermissions = ["public_profile", "email", "user_friends"]
+            loginView.delegate = self
+        }
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -24,6 +34,30 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
-
+    // Facebook Delegate Methods
+    
+    func loginButton(loginButton: FBSDKLoginButton!, didCompleteWithResult result: FBSDKLoginManagerLoginResult!, error: NSError!) {
+        print("User Logged In")
+        
+        if ((error) != nil)
+        {
+            // Process error
+        }
+        else if result.isCancelled {
+            // Handle cancellations
+        }
+        else {
+            // If you ask for multiple permissions at once, you
+            // should check if specific permissions missing
+            if result.grantedPermissions.contains("email")
+            {
+                // Do work
+            }
+        }
+    }
+    
+    func loginButtonDidLogOut(loginButton: FBSDKLoginButton!) {
+        print("User Logged Out")
+    }
 }
 
